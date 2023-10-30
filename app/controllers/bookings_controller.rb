5661aaedb4c743019bf4ed88833e8c7e03573eed
jcampbell57 <jcampbell57@gmail.com>
2023-10-29 20:13:49 -0700

    add booking button to each flight listing, begin creating new booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,20 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    @flight = Flight.find(params[:flight_id])
+    @passenger_count = params[:passengers].to_i
+    # @booking.passengers.build
   end
 
-  def create
-  end
+  def create; end
 
   def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: %i[id name email])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/show'
+  # get 'bookings/new'
+  # get 'bookings/create'
+  # get 'bookings/show'
   root 'flights#index'
   resources :flights, only: :index
+  resources :bookings, only: %i[new create show]
 end
