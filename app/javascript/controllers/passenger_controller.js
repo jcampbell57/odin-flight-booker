9c63311779a51a4d41fd864e67e55684d4e299fb
jcampbell57 <jcampbell57@gmail.com>
2023-11-18 19:58:43 -0800

    allows user to add or remove passengers during booking creation via stimulus

---
+++ app/javascript/controllers/passenger_controller.js
@@ -0,0 +1,42 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="passenger"
+export default class extends Controller {
+  static targets = [ 'error',
+                     'passengerInfo',
+                     'passengerTemplate',
+                     'passengerCount',
+                     'passengerIndex' ]
+
+  connect() {
+    console.log('Hello from passenger controller!')
+    this.passengerCount = this.passengerInfoTarget.childElementCount
+
+  }
+
+  add() {
+    // if statement checks to see if HTML template is supported
+    if ("content" in document.createElement("template")) {
+      this.errorTarget.textContent = ''
+      this.passengerCount++
+
+      const template = this.passengerTemplateTarget;
+      const clone = template.content.cloneNode(true)
+      clone.querySelector('legend').textContent = `Passenger #${this.passengerCount}`
+      this.passengerInfoTarget.appendChild(clone);
+    } else {
+      this.errorTarget.textContent = 'New passenger template not supported.'
+    }
+  } 
+
+  remove() {
+    if (this.passengerCount > 1) {
+      this.errorTarget.textContent = ''
+      this.passengerCountValue--
+
+      this.passengerInfoTarget.children[this.passengerCount - 1].remove()
+    } else {
+      this.errorTarget.textContent = 'There must be at least one passenger.'
+    }
+  }
+}
